b5a0d0b90d61c57c21a3040f617ff6f3b7c5d52f
r3waz <andy.koshmaroff@yandex.ru>
2018-04-14 01:09:05 +0300

    install SASS. good night

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>dorMy</title>
+    <link rel="stylesheet" href="mainstyle.css">
 </head>
 <body>
-    <b>Welcome!</b>
+    <div class="first-block">
+        Welcome to the dorMy!
+    </div>
 </body>
 </html>
