3d440bb71dbdf3eaffdfd0521205de219cd16a4b
r3waz <andy.koshmaroff@yandex.ru>
2018-04-14 23:35:42 +0300

    profile-page

--- components/profile.php
+++ components/profile.php
@@ -28,6 +28,75 @@ include ("header/header.php");
     </div>
     </div>
     <h3>Мои качества</h3>
+    <form action="updateCharacterisric.php" name="characteristic" method="post">
+        <div class="form-block">
+            <b>1. Время сна:</b>  ложусь в
+            <input type="time" name="firstTime">
+            , встаю в
+            <input type="time" name="secondTime"> ;
+        </div>
+        <div class="form-block">
+            <b>2. Чистоплотность (количество принятий душа в неделю): </b>
+            <input type="range" name="washing" min="1" max="14"
+                   onchange="document.getElementById('washingRangeValue').innerHTML = this.value;">
+            <span id="washingRangeValue">10</span>
+        </div>
+        <div class="form-block">
+            <b>3. Общая аккуратность: </b>
+            <input type="range" name="accur" min="1" max="14"
+                   onchange="document.getElementById('accurRangeValue').innerHTML = this.value;">
+            <span id="accurRangeValue">10</span>
+        </div>
+        <div class="form-block">
+            <b>4. Готовность к совместному приготовлению пищи: </b>
+            <input type="range" name="eat" min="1" max="10"
+                   onchange="document.getElementById('eatRangeValue').innerHTML = this.value;">
+            <span id="eatRangeValue">10</span>
+        </div>
+        <div class="form-block">
+            <b>5. Особенности питания:  </b>
+            <div class="styled-select">
+                <select name="eat-special" id="eat-special">
+                    <option value="1">Отсутствуют</option>
+                    <option value="2">Вегетарианец</option>
+                    <option value="3">Веган</option>
+                    <option value="4">Сыроед</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-block">
+            <b>6. Уровень общительности: </b>
+            <input type="range" name="communication" min="1" max="10"
+                   onchange="document.getElementById('communicationRangeValue').innerHTML = this.value;">
+            <span id="communicationRangeValue">10</span>
+        </div>
+        <div class="form-block">
+            <b>7. Темперамент:  </b>
+            <div class="styled-select">
+                <select name="temperement" id="temperement">
+                    <option value="1">Сангвиник</option>
+                    <option value="2">Флегматик</option>
+                    <option value="3">Меланхолик</option>
+                    <option value="4">Холерик</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-block">
+            <b>8. Отношение к учебе: </b>
+            <input type="range" name="learning" min="1" max="10"
+                   onchange="document.getElementById('learningRangeValue').innerHTML = this.value;">
+            <span id="learningRangeValue">10</span>
+        </div>
+        <div class="form-block">
+            <b>9. Ответственность: </b>
+            <input type="range" name="respens" min="1" max="10"
+                   onchange="document.getElementById('respensRangeValue').innerHTML = this.value;">
+            <span id="respensRangeValue">10</span>
+        </div>
+        <div class="form-block">
+            <input type="submit" value="Изменить">
+        </div>
+    </form>
 </div>
 
 </body>
