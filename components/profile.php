ba9136c87815215d8b340f1addfb85936c23a774
r3waz <andy.koshmaroff@yandex.ru>
2018-04-14 22:07:23 +0300

    profile start

---
+++ components/profile.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="header/style.css">
+    <link rel="stylesheet" href="profile.css">
+</head>
+<body>
+<?php
+include ("header/header.php");
+?>
+
+<div class="main-container">
+    <div class="head-row">
+    <img class="avatar" src="../img/ava.svg" alt="">
+    <div class="profile-info">
+        <b>Иванов Иван Иванович</b>
+        <i>ред.</i>
+        <p>19.02.1994</p>
+        <p>Юго-западный государственный университет</p>
+        <p>Общежитие №9</p>
+        <p>Студент, 5 курс</p>
+        <p>Телефон: +79207179644</p>
+    </div>
+    </div>
+    <h3>Мои качества</h3>
+</div>
+
+</body>
+</html>
