42a62ce9b648e785184c62af6b1060b2487d0875
r3waz <andy.koshmaroff@yandex.ru>
2018-04-14 17:23:36 +0300

    second header

--- components/header/header.php
+++ components/header/header.php
@@ -1,3 +1,7 @@
 <div class="header-row">
-
+    <img src="../img/dormylogo.svg" alt="">
+    <button>Выйти</button>
+    <div class="menu-point">Поиск</div>
+    <div class="menu-point">Профиль</div>
+    <div class="menu-point">Главная</div>
 </div>
