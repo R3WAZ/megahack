a0894749038d527dd5fa79aaca340c4525db3956
r3waz <andy.koshmaroff@yandex.ru>
2018-04-14 10:54:50 +0300

    header upd

--- components/header/headerMain.php
+++ components/header/headerMain.php
@@ -1,5 +1,7 @@
 <div class="header-main-row">
     <img src="img/dormylogo.svg" alt="">
+    <div class="button-block">
+        <button>Найти соседа</button>
+    </div>
 
-    <button>Найти соседа</button>
 </div>
