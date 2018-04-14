a607064354953a3ae1646396740ad3835ace4159
r3waz <andy.koshmaroff@yandex.ru>
2018-04-14 19:29:07 +0300

    register tmp

--- components/profile.php
+++ components/profile.php
@@ -15,6 +15,45 @@ include ("header/header.php");
 ?>
 <div class="main-container">
     <h3>Добро пожаловать!<br>Приступим к поиску идеального соседа</h3>
+    <form action="registerRun.php" name="register-form" id="register-form">
+        <div class="form-block">
+            <label for="family">Фамилия:<input type="text" name="family"></label>
+        </div>
+        <div class="form-block">
+            <label for="name">Имя:<input type="text" name="name"></label>
+        </div>
+        <div class="form-block">
+            <label for="fathername">Отчество:<input type="text" name="fathername"></label>
+        </div>
+
+        <div class="gender-block">
+            <input type="radio" name="gender" value="1" id="1">
+            <label for="1">М</label>
+            <input type="radio" name="gender" value="2" id="2">
+            <label for="2">М</label>
+        </div>
+        <div class="date-block">
+            <label for="born-date">Дата рождения:<input type="date" name="born-date"></label>
+        </div>
+
+        <div class="gender-block">
+            <input type="radio" name="status" value="1" id="1">
+            <label for="1">Студент</label>
+            <input type="radio" name="status" value="2" id="2">
+            <label for="2">Абитуриент</label>
+        </div>
+
+        <div class="form-block">
+            <label for="email">E-mail:<input type="email" name="email"></label>
+        </div>
+        <div class="form-block">
+            <label for="phone">Номер телефона:<input type="text" name="phone"></label>
+        </div>
+
+        <input type="submit">
+
+
+    </form>
 </div>
 </body>
 </html>
