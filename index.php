91e88957d5121b9ed952594d1923b264ba68d3d4
r3waz <andy.koshmaroff@yandex.ru>
2018-04-14 16:23:26 +0300

    main-page pre-done

--- index.php
+++ index.php
@@ -77,18 +77,19 @@ if (true)
             <img src="img/step1.svg" alt="">
         </div>
         <div class="col-sm-6 col-xs-12 function-container">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolorum id illum inventore iste natus
-                ratione? Accusamus asperiores expedita, harum molestiae, numquam quae quasi quidem quos tempore
-                temporibus
-                veniam voluptatem!</p>
+            <p><b>Заполнение информации о себе.</b><br> Помимо основной обязательной информации, укажите в своём профиле Ваши
+                предпочтения, привычки и стиль жизни. Данная информация используется сервисом для того, чтобы подобрать
+                для Вас наиболее подходящих
+                сожителей и дать возможность им найти Вас.
+            </p>
         </div>
     </div>
     <div class="function-row">
         <div class="col-sm-6 col-xs-12 function-container">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolorum id illum inventore iste natus
-                ratione? Accusamus asperiores expedita, harum molestiae, numquam quae quasi quidem quos tempore
-                temporibus
-                veniam voluptatem!</p>
+            <p><b>Каждый пункт профиля важен!</b><br> Чем больше информации Вы укажете о себе, тем точнее будут результаты поиска
+                подходящих Вам соседей. Характеристики распределены по группам: питание, отдых, гигиена и т.д., что
+                позволит Вам сконцентрироваться на конкретных особенностях личности.
+            </p>
         </div>
         <div class="col-sm-6 col-xs-12 function-container">
             <img src="img/step2.svg" alt="">
@@ -99,18 +100,18 @@ if (true)
             <img src="img/step3.svg" alt="">
         </div>
         <div class="col-sm-6 col-xs-12 function-container">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolorum id illum inventore iste natus
-                ratione? Accusamus asperiores expedita, harum molestiae, numquam quae quasi quidem quos tempore
-                temporibus
-                veniam voluptatem!</p>
+            <p><b>Опрос для познания себя.</b><br> Если Вам сложно выбрать конкретный пункт персональных качеств и даже плавающие
+                значения не спасают ситуации. Воспользуйтесь размещёнными на сайте опросами, которые помогут Вам решить,
+                насколько вы любите общаться или насколько щепетильны к личному пространству.
+            </p>
         </div>
     </div>
     <div class="function-row">
         <div class="col-sm-6 col-xs-12 function-container">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolorum id illum inventore iste natus
-                ratione? Accusamus asperiores expedita, harum molestiae, numquam quae quasi quidem quos tempore
-                temporibus
-                veniam voluptatem!</p>
+            <p><b>Указание приоритетных характеристик.</b><br> Чем выше приоритет характеристики, тем важнее для Вас наличие у
+                Вашего будущего соседа таких же взглядов на данный жизненный аспект, как и у Вас. Это улучшает поиск и
+                уменьшает время, потраченное на просмотр большого числа подобранных профилей.
+            </p>
         </div>
         <div class="col-sm-6 col-xs-12 function-container">
             <img src="img/step4.svg" alt="">
@@ -121,12 +122,19 @@ if (true)
             <img src="img/step5.svg" alt="">
         </div>
         <div class="col-sm-6 col-xs-12 function-container">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolorum id illum inventore iste natus
-                ratione? Accusamus asperiores expedita, harum molestiae, numquam quae quasi quidem quos tempore
-                temporibus
-                veniam voluptatem!</p>
+            <p><b>Используйте список избранных.</b><br> Если вы хотите обдумать своё решение и со свежими мыслями вернутся к
+                просмотру найденного профиля позже, добавляйте понравившихся людей в список избранных, чтобы не
+                потерять, возможно, лучшего соседа в Вашей жизни.
+            </p>
         </div>
     </div>
+</div>
+<div class="footer">
+    <div class="main-container">
+        <h2>Ещё не нашли своего соседа? Сделайте это прямо сейчас!</h2>
+        <button onclick="window.location='register.php'">Найти соседа</button>
+    </div>
+
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
