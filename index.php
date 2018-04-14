c35e7a8c4d13ed0d62ef955ed1a0f412b073851c
r3waz <andy.koshmaroff@yandex.ru>
2018-04-14 09:04:07 +0300

    bootstrap install

--- index.php
+++ index.php
@@ -6,11 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>dorMy</title>
-    <link rel="stylesheet" href="mainstyle.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" href="css/mainstyle.css">
 </head>
 <body>
     <div class="first-block">
         Welcome to the dorMy!
     </div>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Подключаем Bootstrap JS -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
