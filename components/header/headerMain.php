667053c295ea9da97de623fdbd3cbea3113f7d9e
r3waz <andy.koshmaroff@yandex.ru>
2018-04-14 15:02:46 +0300

    add animation

--- components/header/headerMain.php
+++ components/header/headerMain.php
@@ -1,5 +1,5 @@
 <div class="header-main-row">
-    <img src="img/dormylogo.svg" alt="">
+    <img onclick="window.location='register.php'" src="img/dormylogo.svg" alt="">
     <div class="button-block">
         <button onclick="window.location='register.php'">Найти соседа</button>
     </div>
