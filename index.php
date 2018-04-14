0f078d1b2026b5072a5fd0f2d7076bf941801226
r3waz <andy.koshmaroff@yandex.ru>
2018-04-14 11:24:31 +0300

    content add

--- index.php
+++ index.php
@@ -6,21 +6,32 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>dorMy</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/mainstyle.css">
     <link rel="stylesheet" href="components/header/style.css"
 </head>
 <body>
-    <?php
-        if (true)
-            include ("components/header/headerMain.php");
-    ?>
-    <div class="first-block">
-        Welcome to the dorMy!
-    </div>
+<?php
+if (true)
+    include("components/header/headerMain.php");
+?>
+<div class="main-container">
+    <h1>О сервисе</h1>
+    <p><b>Цель сервиса</b> - связать студентов, подходящих друг другу по стилю жизни, для комфортного совместного проживания в
+        общежитии.</p>
+    <p>Многие студенты при поступлении в вузы не представляют, где, как и с кем они будут жить. К тому же ситуация
+        некоторых становится ещё хуже, потому что весь их опыт жизни с посторонними людьми заканчивается на детских
+        лагерях лагерями. Наш сервис поможет уменьшить количество неопределённости студенческой жизни, по крайней мере в
+        области быта, позволив найти наиболее подходящих соседей.
+    </p>
+    <p>На данный момент нами были найдены похожие сервисы, но задача, которую они решают, скорее относится к
+        материальной, так как они позволяют искать соседей для совместного съёма жилья с поиском наиболее выгодных
+        вариантов без учитывания персональных качеств жильцов.
+    </p>
+</div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Подключаем Bootstrap JS -->
-    <script src="js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- Подключаем Bootstrap JS -->
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
