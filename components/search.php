2a0cb92278153ac00afbd976b97593da155b7587
r3waz <andy.koshmaroff@yandex.ru>
2018-04-15 04:15:07 +0300

    searching page

---
+++ components/search.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="header/style.css">
+    <link rel="stylesheet" href="search.css">
+    <!--<link rel="stylesheet" href="../css/bootstrap.min.css">-->
+</head>
+<body>
+<?php
+include("header/header.php");
+?>
+
+<div class="main-container">
+    <h2>Поиск завершен!<br>Ваши потенциальные соседи:</h2>
+    <div class="pre-card" >
+        <div class="user-card">
+            <img src="../img/ava.svg" alt="">
+            <h3>Иванов Иван Иванович</h3>
+            <p>E-mail: ghtr@mail.ru</p>
+        </div>
+    </div>
+    <div class="pre-card" >
+        <div class="user-card">
+            <img src="../img/ava.svg" alt="">
+            <h3>Иванов Иван Иванович</h3>
+            <p>E-mail: ghtr@mail.ru</p>
+        </div>
+    </div>
+    <div class="pre-card" >
+        <div class="user-card">
+            <img src="../img/ava.svg" alt="">
+            <h3>Иванов Иван Иванович</h3>
+            <p>E-mail: ghtr@mail.ru</p>
+        </div>
+    </div>
+    <button onclick="window.location='../index.php'">Вернуться на главную</button>
+</div>
+
+</body>
+</html>
